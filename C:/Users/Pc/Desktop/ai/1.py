32f4743a36a20f7c893a4e62b4f1b1e9a61effc8
Abrar Shaikh <123534996+Abrar9825@users.noreply.github.com>
2024-12-23 14:24:23 +0530

    Automated commit: Changes detected at 2024-12-23 14:24:21

---
+++ C:/Users/Pc/Desktop/ai/1.py
@@ -0,0 +1,22 @@
+def add():
+    print(10+13)
+
+add()
+def sub():
+    print(10-13)
+
+sub()
+
+def mul():
+    print(10*3)
+
+mul()
+def mul():
+    print(10*3)
+
+di()
+def di():
+    print(10*3)
+
+di()
+
